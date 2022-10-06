e94355ef18c71ab8286b1763b7c831178f283ab2
rjmfalgui2 <rjmfalgui2@gmail.com>
2022-10-06 23:52:19 +0800

    Part 3: Views and Templates

--- mysite/polls/urls.py
+++ mysite/polls/urls.py
@@ -1,6 +1,11 @@
 from django.urls import path
 from . import views
 
+app_name = 'polls'
 urlpatterns = [
-    path('index/', views.index, name='index'),
+    path('', views.index, name='index'),
+    path('<int:question_id>/', views.detail, name='detail'), # ex: /polls/5/
+    path('<int:question_id>/results/', views.results, name='results'), # ex: /polls/5/results/
+    path('<int:question_id>/vote/', views.vote, name='vote'), # ex: /polls/5/vote/
+    path('specifics/<int:question_id>/', views.detail, name='detail'), # added the word 'specifics'
 ]
